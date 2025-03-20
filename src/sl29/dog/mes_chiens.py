18405895e50393ebab62ad40d0ffa0e834dc95b4
ClapZzzzz <cstipon963@icloud.com>
2025-03-20 16:20:41 +0100

    ddzq

---
+++ src/sl29/dog/mes_chiens.py
@@ -0,0 +1,48 @@
+# /!\/!\/!\ garder les imports et autres fioritures qui sont placés avant la classe /!\/!\/!\
+
+class Dog:
+    """
+    Une classe représentant un chien.
+
+    Attributes:
+        _race (str): La race du chien (protected).
+        _sex (str): Le sexe du chien (protected).
+        name (str): Le nom du chien (public).
+    """
+
+    def __init__(self, race: str, sex: str, name: str = "") -> None:
+        """
+        Initialise un chien avec une race, un sexe et un nom.
+
+        Args:
+            race (str): La race du chien.
+            sex (str): Le sexe du chien ('M' ou 'F').
+            name (str, optional): Le nom du chien. Par défaut, une chaîne vide.
+        """
+        self._race = race  # Attribut protégé pour la race
+        self._sex = sex    # Attribut protégé pour le sexe
+        self.name = name   # Attribut public pour le nom
+
+    @property
+    def race(self) -> str:
+        """
+        Retourne la race du chien.
+
+        Returns:
+            str: La race du chien.
+        """
+        return self._race
+
+    @property
+    def sex(self) -> str:
+        """
+        DOCSTRINGS A COMPLETER
+        """
+        # A CODER
+        raise NotImplementedError
+
+    def __str__(self) -> str:
+        """
+        DOCSTRINGS A COMPLETER
+        """
+        return f"Chien: {self.name}, Race: {self._race}, Sexe: {self._sex}
